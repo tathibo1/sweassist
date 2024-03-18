847b6b4b1c8b9b11945de2feb7acd90ec4663969
Thomas Thibodeau <thomasthibodeau@MacBook-Pro.fios-router.home>
2024-03-17 21:12:03 -0400

    handles keyboard interupt with a goodbye message

--- main.py
+++ main.py
@@ -13,29 +13,33 @@ client = OpenAI()
 thread = client.beta.threads.create()
 console = Console()
 
-while True:
-  console.print(f"\nPrompt >>> ", style="bright_green", end='')
-  user_input = input()
-  event_handler = EventHandler()
+try:
+  while True:
+    event_handler = EventHandler()
+    console.print(f"\nPrompt >>> ", style="dark_green", end='')
+    user_input = input()
+    
 
-  if user_input.strip() != "":
-    message = client.beta.threads.messages.create(
-      thread_id=thread.id,
-      role="user",
-      content=user_input
-    )
-
-    # Streaming Response TODO: show partial results using Markdown
-    with client.beta.threads.runs.create_and_stream(
-      thread_id=thread.id,
-      assistant_id=ASSISTANT_ID,
-      instructions="Please refer to the user as Tom. Please format all responses in markdown.",
-      event_handler=event_handler,
-    ) as stream:
-      stream.until_done()
-
-    console.print(
-      Panel(
-        Markdown(event_handler.buffer), title="", padding=1
+    if user_input.strip() != "":
+      message = client.beta.threads.messages.create(
+        thread_id=thread.id,
+        role="user",
+        content=user_input
       )
-    )
+
+      # Streaming Response TODO: show partial results using Markdown
+      with client.beta.threads.runs.create_and_stream(
+        thread_id=thread.id,
+        assistant_id=ASSISTANT_ID,
+        instructions="Please refer to the user as Tom. Please format all responses in markdown.",
+        event_handler=event_handler,
+      ) as stream:
+        stream.until_done()
+
+      console.print(
+        Panel(
+          Markdown(event_handler.buffer), title="", padding=1
+        )
+      )
+except KeyboardInterrupt:
+  console.print("\nGoodbye!")
